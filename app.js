20d1665cbc94efd5081b190c79c54259ff5d2667
musiclab0408 <aldemar.vargas@outlook.com>
2026-04-16 20:45:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -75,3 +75,66 @@ modal.addEventListener("click", (e) => {
     modal.style.display = "none";
   }
 });
+
+unction renderGallery(filteredArray) {
+     const gallery = document.getElementById("gallery");
+     gallery.innerHTML = "";
+
+     filteredArray.forEach((fileName) => {
+          const nameData = fileName.split("-").join(" ").split(".")[0];
+          const [owner, pet] = nameData.split("_");
+
+          if (owner && pet) {
+               const card = document.createElement("div");
+               card.className = "pet-card";
+
+               const img = document.createElement("img");
+               img.src = "./assets/" + fileName;
+               img.alt = pet;
+
+               const info = document.createElement("div");
+               info.className = "pet-info";
+
+               const petName = document.createElement("p");
+               petName.className = "pet-name";
+               petName.textContent = pet.charAt(0).toUpperCase() + pet.slice(1);
+
+               const ownerName = document.createElement("p");
+               ownerName.className = "owner-name";
+               ownerName.textContent = "Owner: " + owner.charAt(0).toUpperCase() + owner.slice(1);
+
+               info.appendChild(petName);
+               info.appendChild(ownerName);
+               card.appendChild(img);
+               card.appendChild(info);
+               gallery.appendChild(card);
+               setTimeout(() => { card.classList.add("visible"); }, 10);
+          }
+     });
+}
+
+const showCatsBtn = document.createElement("button");
+showCatsBtn.textContent = "Show Cats";
+
+const showDogsBtn = document.createElement("button");
+showDogsBtn.textContent = "Show Dogs";
+
+document.querySelector("header").after(showCatsBtn, showDogsBtn);
+
+showCatsBtn.addEventListener("click", () => {
+     const cats = petImages.filter(fileName => fileName.includes("cat")); // boolean includes
+     renderGallery(cats);
+});
+
+showDogsBtn.addEventListener("click", () => {
+     const dogs = petImages.filter(fileName => fileName.includes("dog")); // boolean includes
+     renderGallery(dogs);
+});
+
+const showAllBtn = document.createElement("button");
+showAllBtn.textContent = "Show All";
+document.querySelector("header").after(showAllBtn);
+
+showAllBtn.addEventListener("click", () => {
+     renderGallery(petImages);
+});
